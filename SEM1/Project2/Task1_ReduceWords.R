910069c7c15b09b3f246cf823abd9d324066fb60
decieve4 <s2610921@ed.ac.uk>
2024-11-07 19:43:09 +0000

    reduce

--- SEM1/Project2/Task1_ReduceWords.R
+++ SEM1/Project2/Task1_ReduceWords.R
@@ -1,33 +1,96 @@
 source("SEM1/Project2/reducewords.R")
 source("SEM1/Project2/stylometryfunctions.R")
 
-# 1. setting hyper
-numwords <- 500 #number of words to trim the test set down into
-topic <- 4 #Architecture
-
-# 2. load data
+# 1. load data
+# 1.1 M
 humanM <- loadCorpus("SEM1/Project2/functionwords/functionwords/humanfunctionwords/", "functionwords")
 GPTM <- loadCorpus("SEM1/Project2/functionwords/functionwords/GPTfunctionwords/", "functionwords")
-# 2.1 extract specific topic
-humanfeatures <- humanM$features[[topic]] #select the essays on this particular topic
-GPTfeatures <- GPTM$features[[topic]]
-
-# 3. combine Human and GPT
-features <- rbind(humanfeatures, GPTfeatures) #this is a matrix of both human and GPT essays
-#here i use author=0 for human, and author=1 for ChatGPT
-authornames <- c(rep(0,nrow(humanfeatures)), rep(1,nrow(GPTfeatures)))
-
-# rownames(features) <- as.character(authornames)
-
-# 4. reduce process
-#now reduce the essays down to numwords words
-reducedhumanfeatures <- reducewords(humanfeatures, numwords)
-reducedGPTfeatures <- reducewords(GPTfeatures, numwords)
-reducedfeatures <- rbind(reducedhumanfeatures, reducedGPTfeatures)
-
-# 5. Run Classifier
-for (i in 1:nrow(features)) {
-  train <- features[-i,]
-  test <- reducedfeatures[i,,drop=FALSE] #note that only the test set size changes
-  myKNN(train, test, authornames, k=1)
+# 1.2 features
+humanfeatures <- humanM$features
+GPTfeatures <- GPTM$features
+rhfeatures<-humanM$features[[1]]
+rgfeatures<-GPTM$features[[1]]
+numwords <- 500 #number of words to trim the test set down into
+for(i in 2:length(humanM$features)){
+  rhfeatures<-rbind(rhfeatures,humanM$features[[i]])
+  rgfeatures<-rbind(rgfeatures,GPTM$features[[i]])
 }
+
+
+reducedhumanfeatures <- reducewords(rhfeatures,numwords)
+reducedGPTfeatures <- reducewords(rgfeatures,numwords)
+reducedhumanfeatures.mat <- reducedhumanfeatures[1:100, ]
+reducedGPTfeatures.mat <- reducedGPTfeatures[1:100, ]
+reducedfeatures <- list(reducedhumanfeatures.mat, reducedGPTfeatures.mat)
+humanfeatures <- humanM$features#select the essays on this particular topic
+GPTfeatures <- GPTM$features
+humanfeatures.mat <- do.call(rbind, humanfeatures)
+GPTfeatures.mat <- do.call(rbind, GPTfeatures)
+# 1.3* sample 5 topics
+humanfeatures.mat <- humanfeatures.mat[1:100, ]
+GPTfeatures.mat <- GPTfeatures.mat[1:100, ]
+# 1.4 combine human and GPT to be a list with index 1 (human) and 2 (GPT)
+features <- list(humanfeatures.mat, GPTfeatures.mat)
+
+
+
+traindata <- features
+reducedtraindata<-reducedfeatures
+# 2.2 init prediction list
+DApredictions <- NULL
+KNNpredictions <- NULL
+RFpredictions <- NULL
+truth <- NULL
+# 2.3 start leave-one-out
+for (i in 1:length(traindata)) {
+  for (j in 1:nrow(traindata[[i]])) {
+    
+    # 将当前行的特征作为测试数据
+    cv_testdata <- matrix(reducedtraindata[[i]][j,], nrow=1)
+    
+    # 将 traindata 复制，删除当前行的数据以避免泄漏
+    cv_traindata <- traindata
+    cv_traindata[[i]] <- cv_traindata[[i]][-j, , drop=FALSE]
+    
+    # 防止traindata出现空集
+    if (nrow(cv_traindata[[i]]) == 0){
+      cv_traindata <- cv_traindata[-i]
+    }
+    
+    # 使用 discriminantCorpus 进行分类
+    DA_pred <- discriminantCorpus(cv_traindata, cv_testdata)
+    DApredictions <- c(DApredictions, DA_pred)  # 将预测结果追加到 predictions
+    
+    # 使用 KNNCorpus 进行 KNN 分类
+    KNN_pred <- KNNCorpus(cv_traindata, cv_testdata)
+    KNNpredictions <- c(KNNpredictions, KNN_pred)  # 将KNN预测结果追加到 KNNpredictions
+    
+    # 使用 randomForestCorpus 进行分类
+    RF_pred <- randomForestCorpus(cv_traindata, cv_testdata)
+    RFpredictions <- c(RFpredictions, RF_pred)  # 将 Random Forest 预测结果追加到 RFpredictions
+    
+    # 记录真实类别
+    truth <- c(truth, i)
+  }
+}
+
+# 确保 predictions 和 truth 的因子水平一致
+truth <- factor(truth, levels = sort(unique(truth)))
+DApredictions <- factor(DApredictions, levels = levels(truth))
+KNNpredictions <- factor(KNNpredictions, levels = levels(truth))
+RFpredictions <- factor(RFpredictions, levels = levels(truth))
+
+# 打印交叉验证的结果
+message("Discriminant Analysis (DA) Accuracy: ", sum(DApredictions==truth)/length(truth))
+message("KNN Accuracy: ", sum(KNNpredictions==truth)/length(truth))
+message("Random Forest (RF) Accuracy: ", sum(RFpredictions==truth)/length(truth))
+
+# 混淆矩阵
+message("Confusion Matrix for Discriminant Analysis:")
+print(confusionMatrix(as.factor(DApredictions), as.factor(truth)))
+
+message("Confusion Matrix for KNN:")
+print(confusionMatrix(as.factor(KNNpredictions), as.factor(truth)))
+
+message("Confusion Matrix for Random Forest:")
+print(confusionMatrix(as.factor(RFpredictions), as.factor(truth)))
